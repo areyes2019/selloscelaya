dafbab789d97062106b8116255282cabb7af116b
areyes2019 <reyesabdias@gmail.com>
2024-04-02 18:13:08 -0600

    comienzo con el panel de contro

--- app/Views/Panel/panel.php
+++ app/Views/Panel/panel.php
@@ -7,12 +7,23 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="<?php echo base_url('public/css/menu.css');?>">
+    <title>Panel de control</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
-
+    
+    <div id="menu">
+      <div class="row">
+        <div class="col-md-3">
+          <p class="m-0">Panel de control</p>
+        </div>
+        <div class="col-md-9 d-flex align-items-center">
+          <a href="">Productos</a>
+          <a href="">Cotizaciones</a>
+        </div>
+      </div>
+    </div>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
