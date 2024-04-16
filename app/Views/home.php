78a6cb17b81f379bb91bf6a344327cab0d67e734
areyes2019 <reyesabdias@gmail.com>
2024-04-16 17:59:41 -0600

    cambie algunas eticuqetas alt de img

--- app/Views/home.php
+++ app/Views/home.php
@@ -6,7 +6,7 @@
         <div id="header-carousel" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img class="w-100" src="<?php echo base_url('public/img/baner25.jpg'); ?>" alt="Image">
+                    <img class="w-100" src="<?php echo base_url('public/img/baner25.jpg'); ?>" alt="imagen de mi banner principal con grupos de sellos de goma">
                     <div class="carousel-caption d-flex align-items-center">
                         <div class="container">
                             <div class="row align-items-center justify-content-center justify-content-lg-start">
@@ -83,7 +83,7 @@
             <div class="row g-5">
                 <div class="col-lg-6 pt-4" style="min-height: 400px;">
                     <div class="position-relative h-100 wow fadeIn" data-wow-delay="0.1s">
-                        <img class="position-absolute img-fluid w-100 h-100" src="<?php echo base_url('public/img/expertis.jpg'); ?>" style="object-fit: cover;" alt="">
+                        <img class="position-absolute img-fluid w-100 h-100" src="<?php echo base_url('public/img/expertis.jpg'); ?>" style="object-fit: cover;" alt="grupos de sellos de goma fechadores">
                         <div class="position-absolute top-0 end-0 mt-n4 me-n4 py-4 px-5" style="background: rgba(0, 0, 0, .08);">
                             <h1 class="display-4 text-white mb-0">10 <span class="fs-4">Años de</span></h1>
                             <h4 class="text-white">Experiencia</h4>
