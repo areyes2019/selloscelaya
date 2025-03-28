b7f14e0b60088d0a44996aa7c25ed88b5cb96938
Abdias Reyes <reyesabdias@gmail.com>
2025-03-28 12:31:38 -0600

    Revert "ya quedo el modulo de clientes"
    This reverts commit b074ab69fe6739a4030a13829eb34c6c87b2f7a0.

--- app/Views/Panel/clientes.php
+++ app/Views/Panel/clientes.php
@@ -38,7 +38,8 @@
                                 </form>
                               </div>
                               <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
                               </div>
                             </div>
                           </div>
