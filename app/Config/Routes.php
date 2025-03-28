2c8842162eefd9818a36123245a4bccc68bce13d
Abdias Reyes <reyesabdias@gmail.com>
2025-03-28 07:34:48 -0600

    hacemos un marca tu mundo de prueba

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -29,8 +29,19 @@ $routes->group('',static function($routes){
 	$routes->post('actualizar_cliente', 'admin\Clientes::actualizar');
 	$routes->get('eliminar_cliente/(:num)', 'admin\Clientes::eliminar/$1');
 	
+	/*Proveedores*/
+	$routes->get('proveedores', 'admin\Proveedores::index');
+	$routes->post('nuevo_proveedor', 'admin\Proveedores::nuevo');
+	$routes->get('editar_proveedor/(:num)', 'admin\Proveedores::editar/$1');
+	$routes->post('actualizar_proveedor', 'admin\Proveedores::actualizar');
+	$routes->get('eliminar_proveedor/(:num)', 'admin\Proveedores::eliminar/$1');
+	$routes->get('mostrar_familias/(:num)', 'admin\Proveedores::mostrar_familias/$1');
+	$routes->post('agregar_familia', 'admin\Proveedores::agregar_familia');
+
 	/*Articulos*/
 	$routes->get('articulos', 'admin\Articulos::index');
+	$routes->get('mostrar_articulos', 'admin\Articulos::mostrar');
+	$routes->get('mostrar_articulos_compras/(:num)', 'admin\Articulos::mostrar_compras/$1');
 	$routes->post('nuevo_articulo', 'admin\Articulos::nuevo');
 	$routes->get('editar_articulo/(:num)', 'admin\Articulos::editar/$1');
 	$routes->post('actualizar_articulo', 'admin\Articulos::actualizar');
@@ -38,10 +49,40 @@ $routes->group('',static function($routes){
 
 	/*Cotizaciones*/
 	$routes->get('cotizaciones', 'admin\Cotizaciones::index');
-	$routes->get('nueva_cotizacion/(:num)', 'admin\Cotizaciones::nuevo/$1');
+	$routes->get('nueva_cotizacion/(:num)', 'admin\Cotizaciones::nueva/$1');
+	$routes->get('pagina_cotizador/(:any)', 'admin\Cotizaciones::pagina/$1');
 	$routes->get('editar_cotizacion/(:num)', 'admin\Cotizaciones::editar/$1');
-	$routes->post('actualizar_cotizacion', 'admin\Cotizaciones::actualizar');
+	$routes->get('actualizar_cotizacion/(:num)', 'admin\Cotizaciones::actualizar/$1');
 	$routes->get('eliminar_cotizacion/(:num)', 'admin\Cotizaciones::eliminar/$1');
+	$routes->post('agregar_articulo', 'admin\Cotizaciones::agregar');
+	$routes->post('agregar_articulo_ind', 'admin\Cotizaciones::agregar_ind');
+	$routes->get('mostrar_detalles/(:num)', 'admin\Cotizaciones::mostrar_detalles/$1');
+	$routes->get('borrar_linea/(:num)', 'admin\Cotizaciones::borrar_linea/$1');
+	$routes->get('descargar_cotizacion/(:num)', 'admin\Cotizaciones::cotizacion_pdf/$1');
+	$routes->get('enviar', 'admin\Cotizaciones::enviar');
+	$routes->get('enviar_pdf/(:num)', 'admin\Cotizaciones::enviar_pdf/$1');
+	$routes->post('pago', 'admin\Cotizaciones::pago');
+	$routes->post('modificar_cantidad', 'admin\Cotizaciones::modificar_cantidad');
+	$routes->post('marcar_entregado', 'admin\Cotizaciones::entregado');
+
+	/*Compras*/
+	$routes->get('compras', 'admin\Compras::index');
+	$routes->get('mostrar_compras', 'admin\Compras::mostrar');
+	$routes->get('pedido/(:num)', 'admin\Compras::pedido/$1');
+	$routes->get('nueva_compra/(:num)', 'admin\Compras::nueva/$1');
+	$routes->get('pagina_orden/(:any)', 'admin\Compras::pagina/$1');
+	$routes->get('editar_compra/(:num)', 'admin\Compras::editar/$1');
+	$routes->get('actualizar_compra/(:num)', 'admin\Compras::actualizar/$1');
+	$routes->get('eliminar_compra/(:num)', 'admin\Compras::eliminar/$1');
+	$routes->post('agregar_articulo_compras', 'admin\Compras::agregar');
+	$routes->get('mostrar_detalles_compras/(:num)', 'admin\Compras::mostrar_detalles/$1');
+	$routes->get('borrar_linea_compras/(:num)', 'admin\Compras::borrar_linea/$1');
+	$routes->get('descargar_orden/(:num)', 'admin\Compras::cotizacion_pdf/$1');
+	$routes->get('enviar_orden', 'admin\Compras::enviar');
+	$routes->get('enviar_pdf_orden/(:num)', 'admin\Compras::enviar_pdf/$1');
+	$routes->post('pago_compras', 'admin\Compras::pago');
+	$routes->post('compra_recibida', 'admin\Compras::recibida');
+	$routes->post('modificar_cantidad_compras', 'admin\Compras::modificar_cantidad');
 
 	/*Cuentas*/
 	$routes->get('contabilidad', 'admin\Contabiidad::index');
@@ -50,7 +91,28 @@ $routes->group('',static function($routes){
 	$routes->post('actualizar_cuenta', 'admin\Contabiidad::actualizar');
 	$routes->get('eliminar_cuenta/(:num)', 'admin\Contabiidad::eliminar/$1');
 
+	
+	/*Pedidos*/
+	$routes->get('pedidos', 'admin\Pedidos::index');
+	$routes->get('nuevo_pedido/(:num)', 'admin\Pedidos::nueva/$1');
+	$routes->get('pagina_pedido/(:any)', 'admin\Pedidos::pagina/$1');
+	$routes->get('editar_pedido/(:num)', 'admin\Pedidos::editar/$1');
+	$routes->get('actualizar_pedido/(:num)', 'admin\Pedidos::actualizar/$1');
+	$routes->get('eliminar_pedido/(:num)', 'admin\Pedidos::eliminar/$1');
+	$routes->post('agregar_articulo_pedido', 'admin\Pedidos::agregar');
+	$routes->get('mostrar_detalles_pedido/(:num)', 'admin\Pedidos::mostrar_detalles/$1');
+	$routes->get('borrar_linea_pedido/(:num)', 'admin\Pedidos::borrar_linea/$1');
+	$routes->get('descargar_pedido/(:num)', 'admin\Pedidos::cotizacion_pdf/$1');
+	$routes->get('enviar_pedido', 'admin\Pedidos::enviar');
+	$routes->get('enviar_pdf_pedido/(:num)', 'admin\Pedidos::enviar_pdf/$1');
+	$routes->post('pago_pedido', 'admin\Pedidos::pago');
 
+	/*Existencias*/
+	$routes->get('existencias', 'admin\Existencias::index');
+	$routes->get('nueva_existencia/(:num)', 'admin\Existencias::nuevo/$1');
+	$routes->get('editar_existencia/(:num)', 'admin\Existencias::editar/$1');
+	$routes->post('actualizar_existencia', 'admin\Existencias::actualizar');
+	$routes->get('eliminar_existencia/(:num)', 'admin\Existencias::eliminar/$1');
 
 	$routes->get('nueva_venta', 'admin\Ventas::nueva');
 	$routes->get('cotizaciones', 'admin\Cotizaciones::index');
