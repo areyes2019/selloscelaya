6cc415bcf801a80166eb03bf650cffbc3709c848
areyes2019 <reyesabdias@gmail.com>
2026-04-01 12:28:46 -0600

    arreglamos las rutas

--- app/Views/Panel/administracion.php
+++ app/Views/Panel/administracion.php
@@ -428,6 +428,7 @@
 <script>
     window.csrfToken = "<?= csrf_token() ?>";
     window.csrfHash = "<?= csrf_hash() ?>";
+    window.baseUrl = "<?= base_url() ?>";
 </script>
 <script src="<?php echo base_url('public/js/admin.js'); ?>"></script>
 <?php echo $this->endSection()?>
